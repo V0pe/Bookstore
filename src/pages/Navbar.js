6b3466db40e2d12342ffb9c107fb3943c1901924
V0pe <dampblew01@gmail.com>
2022-06-04 03:36:56 -0400

    add the components and pages dir

---
+++ src/pages/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './pages.css';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'BOOK',
+    },
+    {
+      id: 2,
+      path: '/CategoryPage',
+      text: 'CATEGORIES',
+    },
+  ];
+
+  return (
+    <header>
+      <h1></h1>
+      <nav className="navBar">
+        <ul>
+          {links.map((link) => (
+            <li key={link.id}>
+              <NavLink to={link.path}>
+                {link.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default Navbar;
