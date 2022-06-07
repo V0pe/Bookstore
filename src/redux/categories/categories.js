8ff501c022c3d02a8a58ec8ba0592d4ce5f2717d
V0pe <dampblew01@gmail.com>
2022-06-07 09:13:21 -0400

    add category reducer

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,14 @@
+export const checkStatus = () => ({
+    type: 'STATUSCHECK',
+  });
+
+  const categoriesReducer = (categories = [], action) => {
+    switch (action.type) {
+      case 'STATUSCHECK':
+        return 'Under Construction';
+      default:
+        return categories;
+    }
+  };
+  
+  export default categoriesReducer;
