5881713dc0ce38e0fdd3c27cd9d7381f77d844bc
V0pe <dampblew01@gmail.com>
2022-06-07 09:38:54 -0400

    follow duck pattern

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,18 +1,18 @@
 export const addBook = (book) => ({
-  type: 'ADDBOOK',
+  type: 'bookstore/books/ADD_BOOK',
   payload: book,
 });
 
 export const removeBook = (book) => ({
-  type: 'REMOVEBOOK',
+  type: 'bookstore/books/REMOVE_BOOK',
   payload: book,
 });
 
 const booksReducer = (books = [], action) => {
   switch (action.type) {
-    case 'ADDBOOK':
+    case 'bookstore/books/ADD_BOOK':
       return [...books, action.payload];
-    case 'REMOVEBOOK':
+    case 'bookstore/books/REMOVE_BOOK':
       return books.filter((book) => book.id !== action.payload.id);
     default:
       return books;
--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,10 +1,10 @@
 export const checkStatus = () => ({
-  type: 'STATUSCHECK',
+  type: 'bookstore/categories/STATUS_CHECK',
 });
 
 const categoriesReducer = (categories = [], action) => {
   switch (action.type) {
-    case 'STATUSCHECK':
+    case 'bookstore/categories/STATUS_CHECK':
       return 'Under Construction';
     default:
       return categories;
