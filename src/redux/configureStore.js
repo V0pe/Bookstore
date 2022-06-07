7d728ad2c5cf2b12b1ddff1356a023fa0b3bbd38
V0pe <dampblew01@gmail.com>
2022-06-07 09:16:10 -0400

    created the store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,22 +1,11 @@
-export const addBook = (book) => ({
-    type: 'ADDBOOK',
-    payload: book,
-  });
-  
-  export const removeBook = (book) => ({
-    type: 'REMOVEBOOK',
-    payload: book,
-  });
-  
-  const booksReducer = (books = [], action) => {
-    switch (action.type) {
-      case 'ADDBOOK':
-        return [...books, action.payload];
-      case 'REMOVEBOOK':
-        return books.filter((book) => book.id !== action.payload.id);
-      default:
-        return books;
-    }
-  };
-  
-  export default booksReducer;
+import { combineReducers, createStore } from 'redux';
+import booksReducer from './books/books';
+import categoriesReducer from './categories/categories';
+
+const reducers = {
+  books: booksReducer,
+  categories: categoriesReducer,
+};
+
+const store = createStore(combineReducers(reducers));
+export default store;
